0f53f1713d904ad32da8f114976602143abcf4b8
David Pires <developer@davidpires.pt>
2024-06-09 01:12:34 +0100

    Fix documentation

--- mod.ts
+++ mod.ts
@@ -1,3 +1,14 @@
 /**
  * @module Result
+ *
+ * @example
+ * ```ts
+ * import { Result, ok, err } from '@nnou/result';
+ *
+ * const result = ok(42); // Returns Result<number, never>
+ * ```
  */
+
+export * from './assert.ts';
+export * from './result.ts';
+export * from './resolve.ts';
